54544761f00b86fb766d063c9f27ebdeb11a1355
Syreeta Monroe <syreeta.monroe@gmail.com>
2018-06-20 09:51:43 -0400

    not quite finished

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+
+// var numberOptions = [10, 5, 3, 7];
+// var winCounter = 0;
+// var lossCounter = 0;
+// var targetNumber = Math.floor(Math.random() * 101);
+// $("#targetNumber").text(targetNumber);
+
+
+//  function reset(){
+//     numberOptions = number[Math.floor(Math.random() * 120)];
+// }
+
+
+
+
+// $("#number-to-guess").text(targetNumber);
