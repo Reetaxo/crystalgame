0025bbec13a7ef8b1db9f746b119a6561d8321f9
Syreeta Monroe <syreeta.monroe@gmail.com>
2018-06-21 23:44:00 -0400

    nothing works no matter what i've changed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,41 @@
+// var numberOptions = [10, 5, 3, 7]; number options are no longer needed if guessing random
 
-// var numberOptions = [10, 5, 3, 7];
-// var winCounter = 0;
-// var lossCounter = 0;
-// var targetNumber = Math.floor(Math.random() * 101);
-// $("#targetNumber").text(targetNumber);
+var winCounter = 0;
+var lossCounter = 0;
+var targetNumber = Math.floor(Math.random() * 120);
+$("#number-to-guess").text(targetNumber);
 
+var counter = 0;
 
-//  function reset(){
-//     numberOptions = number[Math.floor(Math.random() * 120)];
-// }
+// We begin by expanding our array to include four options.
+var gin1 = Math.floor(Math.random() * 12);
+var gin2 = Math.floor(Math.random() * 12);
+var gin3 = Math.floor(Math.random() * 12);
+var gin4 = Math.floor(Math.random() * 12);
+
+$("#winCounter").html(winCounter);
+$("#lossCounter").html(lossCounter);
+
+// var imageCrystal = $("<img>");
+
+function reset(){
+
+}
+
+alert("New score: " + counter);
+
+    if (counter === targetNumber) {
+      alert("You win!");
+    }
+
+    else if (counter >= targetNumber) {
+      alert("You lose!!");
+    }
+
+function reset(){
+    if (counter >= targetNumber) {
+      var counter = 0;
+    }
 
 
 
